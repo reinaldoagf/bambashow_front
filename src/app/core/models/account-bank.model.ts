11a31b97124a7e5967ad8ed6b0f124273b450ca6
unknown <reinaldoagf1@gmail.com>
2022-12-04 18:24:42 -0400

    feat: bank accounts settings

---
+++ src/app/core/models/account-bank.model.ts
@@ -0,0 +1,16 @@
+export class AccountBank {
+    id: number;
+    bank:string;
+    account_number:string;
+    email:string;
+    identity_card:string;
+    phone:string;
+    constructor(){
+        this.id=null;
+        this.bank=null;
+        this.account_number=null;
+        this.email=null;
+        this.identity_card=null;
+        this.phone=null;
+    }
+}
--- src/app/pages/settings/bank-data/bank-data.component.ts
+++ src/app/pages/settings/bank-data/bank-data.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AdminNavbarService } from 'src/app/core/services/admin-navbar.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
+import { RestService } from 'src/app/core/services/rest.service';
+import { AccountBank } from 'src/app/core/models/account-bank.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-bank-data',
@@ -6,10 +12,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bank-data.component.css']
 })
 export class BankDataComponent implements OnInit {
+  accountBank:AccountBank=new AccountBank();
+  accountsBank:AccountBank[]=[];
 
-  constructor() { }
+  constructor(
+    private spinner: NgxSpinnerService,
+    private restService: RestService,
+    private adminNavbarService: AdminNavbarService,
+    private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    this.adminNavbarService.changePage({
+      path: '/admin/settings/social-networks',
+      breadcumbs: ['Configuración', 'cUENTAS BANCARIAS']
+    })
+    this.getData()
   }
 
+  async getData() {
+    try {
+      this.spinner.show();
+      const [
+        response1,
+      ]: any[] = await Promise.all([
+        this.restService.get(`/settings/accounts-bank`),
+      ]);
+      this.spinner.hide();
+      this.accountsBank = response1.data ? response1.data : [];
+
+    } catch (error) {
+      this.spinner.hide();
+      console.log(error);
+    }
+  }
+  async save() {
+    try {
+      if(this.accountBank.id){
+        this.spinner.show();
+        const response: any = this.restService.put(`/settings/accounts-bank/update/${this.accountBank.id}`,this.accountBank);
+        this.spinner.hide();
+        this.accountBank = response.data ? response.data : this.accountBank;
+        this.notificationService.showSuccess('Operación realiza exitosamente', response.message)
+      }
+    } catch (error) {
+      this.spinner.hide();
+      console.log(error);
+    }
+  }
+  edit(item: any){
+    this.accountBank = Object.assign({},item)
+  }
 }
